8af5c2a09820e174346baa8f8555b7a37f280b9b
CarlosCVG <143562985+CarlosCVG@users.noreply.github.com>
2023-08-29 23:19:09 -0600

    Add files via upload

---
+++ EVA1_1_HOLA_MUNDO/src/main/java/com/mycompany/eva1_1_hola_mundo/EVA1_1_HOLA_MUNDO.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_1_hola_mundo;
+
+/**
+ *
+ * @author Vigoc
+ */
+public class EVA1_1_HOLA_MUNDO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+        System.out.println("Buenos dias");
+    }
+}
